072c7281345f1a3932fb0e6d6729d4bce163cfc6
PatriceJiang <397136899@qq.com>
2020-12-13 02:16:35 +0800

    save

--- compose/mount/data/script/startup.sh
+++ compose/mount/data/script/startup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash 
 set -x
-
+scl enable php72 bash
 rabbitmq-server start -detached
+sleep 3
 rabbitmq-plugins enable rabbitmq_management
 rabbitmq-plugins enable rabbitmq_top
+
+php-fpm -c /etc -D
 
 # redis-server --daemonize yes
 # /usr/bin/memcached -d
